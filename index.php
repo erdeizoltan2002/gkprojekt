4c73d97d193426d09f135883b2e648168b50c2e3
krisztiann2 <94106318+krisztiann2@users.noreply.github.com>
2023-01-07 12:03:02 +0100

    Add files via upload
    Mainground

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    $db = new mysqli('localhost','root','','');
+    if(isset($_POST['nev']) && isset($_POST['jelszo'])){
+        $adatok = $db -> query("SELECT * from felhasznalok WHERE felhnev= '".$_POST['nev']."' AND jelszo= '".$_POST['jelszo']."'");
+        if($adatok -> fetch_assoc()){
+            $_SESSION['nev'] = $_POST['nev'];
+        }else {
+            echo"Sikertelen bejelentkezés";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Főoldal</title>
+</head>
+<body>
+    <?php
+    //login
+        if(!isset($_SESSION['nev'])){
+
+    ?>
+    <form method="post">
+        <label for="nev">Név:</label><br>
+        <input type="text" name="nev"><br>
+        <label for="jelszo">Jelszó:</label><br>
+        <input type="password" name="jelszo"><br>
+        <button type="submit">Bejelentkezés</button>
+    </form>
+    <?php
+        }
+    ?>
+</body>
+</html>
