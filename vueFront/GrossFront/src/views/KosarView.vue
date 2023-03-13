621499d302dc78e1ca5646abf4f8ecf6b21f2b71
krisztiann3 <97217377+krisztiann3@users.noreply.github.com>
2023-03-13 12:06:58 +0100

    Kosár elkészítése
    KosarView.Vue elkészíítése, hozzá tartozó utvonallal (/Kosar)

---
+++ vueFront/GrossFront/src/views/KosarView.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import axios from 'axios'
+    import { onMounted,ref } from 'vue';
+    import { useRoute,useRouter } from 'vue-router';
+
+    const route = useRoute()
+    const router = useRouter()
+
+</script>
+
+<template>
+
+</template>
